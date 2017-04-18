4a74e1dee296f43e600ea6d709a87cc5e9d39fb2
Giliwawa <alaa.ksontini@esprit.tn>
2017-04-18 14:00:15 +0100

    fix

--- src/lib/enrichementToUser.js
+++ src/lib/enrichementToUser.js
@@ -1,22 +1,22 @@
  let userTransformer = (enrichement) => {
   let user = {};
-  
+
   user.user_info = {
     email : enrichement.person.email,
     first_name: enrichement.person.name.givenName,
     last_name : enrichement.person.name.familyName,
     occupation: "",
     company : enrichement.company.name,
-    bio : enrichement.person.bio,
+    bio : enrichement.person.bio || "",
     social:[
       {
-        twitter : enrichement.person.twitter.handle
+        twitter : enrichement.person.twitter.handle || ""
       },
       {
-        facebook : enrichement.person.facebook.handle
+        facebook : enrichement.person.facebook.handle || ""
       }
     ],
-    profile_img: enrichement.person.avatar
+    profile_img: enrichement.person.avatar || ""
   }
 
   return user;
