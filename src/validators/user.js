b2134a787a2a29c16f892a85f55947ab5f3d9936
Giliwawa <alaa.ksontini@esprit.tn>
2017-04-18 16:54:33 +0100

    fixed login vlidator

--- src/validators/user.js
+++ src/validators/user.js
@@ -22,21 +22,21 @@ const entityValidator = {
 
 }
 const loginValidator = {
-  "user_info": {
-    'email': {
-      notEmpty: true,
-      isEmail:{
-        errorMessage: "you must provide an email"
-      }
-    }
-  },
-  'password':{
+
+  'email': {
     notEmpty: true,
-    matches: {
-      options: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/]
-    },
-    errorMessage: "password must contain at least 8 characters with at least one lowercase, at least one uppercase and at least a number"
+    isEmail:{
+      errorMessage: "you must provide an email"
+    }
   }
+},
+'password':{
+  notEmpty: true,
+  matches: {
+    options: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/]
+  },
+  errorMessage: "password must contain at least 8 characters with at least one lowercase, at least one uppercase and at least a number"
+
 }
 
 export default {
