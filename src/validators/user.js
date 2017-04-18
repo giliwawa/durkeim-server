f4dfa079d801d84ee1d1035d4c9e743fc2a8fad9
Giliwawa <alaa.ksontini@esprit.tn>
2017-04-18 17:08:59 +0100

    more validation schema fixing

--- src/validators/user.js
+++ src/validators/user.js
@@ -23,18 +23,19 @@ const entityValidator = {
 }
 const loginValidator = {
 
-'email': {
-  notEmpty: true,
-  isEmail:{
-    errorMessage: "you must provide an email"
-  }
-},
-'password':{
-  notEmpty: true,
-  matches: {
-    options: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/]
+  'email': {
+    notEmpty: true,
+    isEmail:{
+      errorMessage: "you must provide an email"
+    }
   },
-  errorMessage: "password must contain at least 8 characters with at least one lowercase, at least one uppercase and at least a number"
+  'password':{
+    notEmpty: true,
+    matches: {
+      options: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/]
+    },
+    errorMessage: "password must contain at least 8 characters with at least one lowercase, at least one uppercase and at least a number"
+  }
 
 }
 
