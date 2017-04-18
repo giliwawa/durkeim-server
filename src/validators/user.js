8d807eb6168df7da2d2a59b6250b0299b0ea6d24
Giliwawa <alaa.ksontini@esprit.tn>
2017-04-18 14:16:09 +0100

    fix validator

--- src/validators/user.js
+++ src/validators/user.js
@@ -1,25 +1,24 @@
 const entityValidator = {
-  "user_info" : {
-    'email': {
-      notEmpty: true,
-      isEmail:{
-        errorMessage: "you must provide an email"
-      }
-    },
-    'first_name':{
-      notEmpty: true,
-    },
-    'last_name':{
-      notEmpty: true,
+
+  user_info.email: {
+    notEmpty: true,
+    isEmail:{
+      errorMessage: "you must provide an email"
     }
   },
-  'password':{
+  user_info.first_name:{
     notEmpty: true,
-    matches: {
-      options: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/]
-    },
-    errorMessage: "password must contain at least 8 characters with at least one lowercase, at least one uppercase and at least a number"
   },
+  user_info.last_name:{
+    notEmpty: true,
+  }
+},
+'password':{
+  notEmpty: true,
+  matches: {
+    options: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/]
+  },
+  errorMessage: "password must contain at least 8 characters with at least one lowercase, at least one uppercase and at least a number"
 
 }
 const loginValidator = {
